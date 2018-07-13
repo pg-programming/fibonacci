ec30fd6ff654edbf88de8e5ca9aeab82bce6cce0
Reed Wilson <rwilson@ancestry.com>
2018-07-13 11:07:34 -0600

    python3ify

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
 os.system('clear')
-option = int(raw_input('Please enter your choice:\n1) nth Fibonacci number\n2) last Fibonacci number <= n\n3) quit\n'))
+message = '''Please enter your choice:
+1) nth Fibonacci number
+2) last Fibonacci number <= n
+3) quit
+'''
+option = int(input(message))
 
 if option == 1 or option == 2:
-    number = int(raw_input("\nPlease enter the number: "))
+    number = int(input("\nPlease enter the number: "))
     # IMPLEMENT OPTIONS 1 AND 2 HERE
 elif option == 3:
     os.system('clear')
     sys.exit()
 else:
-    print "I didn't understand your option"
+    print("I didn't understand your option")
 
-raw_input('\nPress enter to quit...')
+input('\nPress enter to quit...')
 os.system('clear')
